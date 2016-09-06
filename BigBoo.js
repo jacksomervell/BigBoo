d0898172275256963974747c96839e0c984e4eea
Jack Somervell <jacksomervell@gmail.com>
2016-09-06 22:30:50 +0100

    little tweaks

--- BigBoo.js
+++ BigBoo.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
 
 var size = 0
+var score = 0
 
 $('.bananas').mouseenter(function(){
-
   size = size+1
-
+  score = score+1
   $('#container').css('cursor', 'url(images/boosmall10.gif) 25 15, pointer');
-  $('.bananas').css('display', 'none');
-
+  $(this).css('display', 'none');
   console.log(size);
+
 })
 
 
+ $('#container').append('<div class="banana"></div>')
 
 
 
