d166b069454bf193692a2c6de5dc4078252b9bb7
Jack Somervell <jacksomervell@gmail.com>
2016-09-11 16:13:27 +0100

    bananas now hovering
    '

--- BigBoo.js
+++ BigBoo.js
@@ -13,27 +13,38 @@ $('.bananas').mouseenter(function(){
 })
 
 function makeBanana(){
-  var height = Math.random()*1000
-   $('#container').append('<div class="bananas" style="top:' + height + '"px></div>')
+  var height = Math.random()*100;
+  height = Math.round(height);
+   $('#container').append('<div class="bananas" style="top:' + height + '%; left:0px"></div>');
+
+   $('.bananas').mouseenter(function(){
+  size = size+1
+  score = score+1
+  $('#container').css('cursor', 'url(images/boosmall10.gif) 25 15, pointer');
+  $(this).css('display', 'none');
+  console.log(size);
+
+})
 
   }
 
+// function moveIt(){
+//   var banana = $('.bananas');
+//   var currentLeft = parseInt(banana.css('left'));
+//   var newLeft = currentLeft + 1;
+//   banana.css('left', newLeft);
+// }
 
- var banana = $('.bananas');
-
-function moveBanana(image){
-  setInterval(moveIt(image), 500)
-  setInterval(console.log('hello'))
-}
-
-function moveIt(){
+function moveBanana(){
+  $('.bananas').each(function(index){
+    var banana = $(this);
   var currentLeft = parseInt(banana.css('left'));
   var newLeft = currentLeft + 1;
-  banana.css('left', newLeft);
+  banana.css('left', newLeft);  })
 }
 
 
-
-setInterval(makeBanana, 10);
-setInterval(moveIt, 10);
+setInterval(makeBanana, 1000);
+// setInterval(moveIt, 10);
+setInterval(moveBanana, 10);
 })
