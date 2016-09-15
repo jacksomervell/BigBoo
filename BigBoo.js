7016efcefc5353f8a8f30245b763ece62a4ee31d
Jack Somervell <jacksomervell@gmail.com>
2016-09-15 21:41:49 +0100

    added collision stuff and all good. Now need to change boo divs to boo images

--- BigBoo.js
+++ BigBoo.js
@@ -5,69 +5,94 @@ var gameOver = false
 
 $(document).ready(function(){
 
+  //when banana is touched:
+
+  function bananaCollide(){
+    var collide = $('.boo').collision('.bananas');
+    if (collide[0]){
+      collide.remove()
+    size = size+1
+    score = score+1
+    $('#score').text('Score: ' + score)
+    }
+  }
+
+//when mushroom is touched
+  function mushroomCollide(){
+    var mushcollide = $('.boo').collision('.mushroom');
+    if(mushcollide[0]){ 
+      console.log('boo')
+  // $('#container').append('<div class = "game-over"></div>')
+  $('.bananas').remove();
+  // $('.mushroom').remove();
+  clearInterval(makingInterval);
+  clearInterval(bananaMover);
+  clearInterval(mushroomMover);
+    }
+  }
 
 //this function makes everything:
 //yesorNo is the probability it will appear
 
-function makeBanana(){
+  function makeBanana(){
 
-  var height = Math.random()*100;
-  height = Math.round(height);
-  var yesOrNo = Math.random()
+    var height = Math.random()*100;
+    height = Math.round(height);
+    var yesOrNo = Math.random()
 
- if (yesOrNo > 0.2){
-  //make left to right banana
-   $('#container').append('<div class="bananas left-banana" style="top:' + height + '%; left:-50px"></div>');
- }
+   if (yesOrNo > 0.2){
+    //make left to right banana
+     $('#container').append('<div class="bananas left-banana" style="top:' + height + '%; left:-50px"></div>');
+   }
    //make right to left banana
 
-  var rightHeight = Math.random()*100;
-  rightHeight = Math.round(rightHeight);
-  var yesOrNo = Math.random()
+    var rightHeight = Math.random()*100;
+    rightHeight = Math.round(rightHeight);
+    var yesOrNo = Math.random()
 
-  if (yesOrNo > 0.2){
-   $('#container').append('<div class="bananas right-banana" style="top:' + rightHeight + '%; right:-50px"></div>');
- }
+    if (yesOrNo > 0.2){
+     $('#container').append('<div class="bananas right-banana" style="top:' + rightHeight + '%; right:-50px"></div>');
+   }
 
  //make top to bottom banana
 
- var topWidth = Math.random()*100;
-  topWidth = Math.round(topWidth);
-  var yesOrNo = Math.random()
+   var topWidth = Math.random()*100;
+    topWidth = Math.round(topWidth);
+    var yesOrNo = Math.random()
 
-  if (yesOrNo > 0.2){
-   $('#container').append('<div class="bananas top-banana" style="left:' + topWidth + '%; top:-50px"></div>');
- }
+    if (yesOrNo > 0.2){
+     $('#container').append('<div class="bananas top-banana" style="left:' + topWidth + '%; top:-50px"></div>');
+   }
 
  //make bottom to top banana:
 
-var bottomWidth = Math.random()*100;
-  bottomWidth = Math.round(bottomWidth);
-  var yesOrNo = Math.random()
+  var bottomWidth = Math.random()*100;
+    bottomWidth = Math.round(bottomWidth);
+    var yesOrNo = Math.random()
 
-  if (yesOrNo > 0.2){
-   $('#container').append('<div class="bananas bottom-banana" style="left:' + bottomWidth + '%; bottom:-50px"></div>');
- }
+    if (yesOrNo > 0.2){
+     $('#container').append('<div class="bananas bottom-banana" style="left:' + bottomWidth + '%; bottom:-50px"></div>');
+   }
 
  //make left-to-right mushroom
 
-  var leftHeight = Math.random()*100;
-  leftHeight = Math.round(leftHeight);
-  var yesOrNo = Math.random()
+    var leftHeight = Math.random()*100;
+    leftHeight = Math.round(leftHeight);
+    var yesOrNo = Math.random()
 
-  if (yesOrNo > 0.5){
-   $('#container').append('<div class="mushroom left-mushroom" style="top:' + leftHeight + '%; left:-50px"></div>');
- }
+    if (yesOrNo > 0.5){
+     $('#container').append('<div class="mushroom left-mushroom" style="top:' + leftHeight + '%; left:-50px"></div>');
+   }
 
 //make right-to-left mushroom
 
-  var rightHeight = Math.random()*100;
-  rightHeight = Math.round(rightHeight);
-  var yesOrNo = Math.random()
+    var rightHeight = Math.random()*100;
+    rightHeight = Math.round(rightHeight);
+    var yesOrNo = Math.random()
 
-  if (yesOrNo > 0.5){
-   $('#container').append('<div class="mushroom right-mushroom" style="top:' + rightHeight + '%; right:-50px"></div>');
- }
+    if (yesOrNo > 0.5){
+     $('#container').append('<div class="mushroom right-mushroom" style="top:' + rightHeight + '%; right:-50px"></div>');
+   }
 
  //make top-to-bottom mushroom
 
@@ -90,76 +115,56 @@ var bottomWidth = Math.random()*100;
  }
 
 
-
- //when any banana is touched:
-   $('.bananas').mouseenter(function(){
-  size = size+1
-  score = score+1
-  $(this).css('display', 'none');
-  $('#score').text('Score: ' + score)
-  })
-
- //when any mushroom is touched
-   $('.mushroom').mouseenter(function(){
-  $(this).css('display', 'none');
-  $('#container').append('<div class = "game-over"></div>')
-  $('.bananas').remove();
-  $('.mushroom').remove();
-  clearInterval(makingInterval);
-  clearInterval(bananaMover);
-  clearInterval(mushroomMover);
-  })
-
 }
 
 //this moves all bananas!
 
-function moveBanana(){
-  $('.left-banana').each(function(index){
-    var banana = $(this);
-  var currentLeft = parseInt(banana.css('left'));
-  var newLeft = currentLeft + 1;
-  banana.css('left', newLeft);
+  function moveBanana(){
+    $('.left-banana').each(function(index){
+      var banana = $(this);
+    var currentLeft = parseInt(banana.css('left'));
+    var newLeft = currentLeft + 1;
+    banana.css('left', newLeft);
 
-  if (currentLeft > 950){
-    banana.remove();
-  }
+    if (currentLeft > 950){
+      banana.remove();
+    }
 
   })
 
   $('.right-banana').each(function(index){
-    var banana = $(this);
-  var currentRight = parseInt(banana.css('right'));
-  var newRight = currentRight + 1;
-  banana.css('right', newRight);
+      var banana = $(this);
+    var currentRight = parseInt(banana.css('right'));
+    var newRight = currentRight + 1;
+    banana.css('right', newRight);
 
-  if (currentRight > 950){
-    banana.remove();
-  }
+    if (currentRight > 950){
+      banana.remove();
+    }
 
   })
 
   $('.top-banana').each(function(index){
     var banana = $(this);
-  var currentTop = parseInt(banana.css('top'));
-  var newTop = currentTop + 1;
-  banana.css('top', newTop);
+    var currentTop = parseInt(banana.css('top'));
+    var newTop = currentTop + 1;
+    banana.css('top', newTop);
 
-  if (currentTop > 950){
-    banana.remove();
-  }
+    if (currentTop > 950){
+      banana.remove();
+    }
 
   })
 
    $('.bottom-banana').each(function(index){
     var banana = $(this);
-  var currentBottom = parseInt(banana.css('bottom'));
-  var newBottom = currentBottom + 1;
-  banana.css('bottom', newBottom);
+    var currentBottom = parseInt(banana.css('bottom'));
+    var newBottom = currentBottom + 1;
+    banana.css('bottom', newBottom);
 
-  if (currentBottom > 950){
-    banana.remove();
-  }
+    if (currentBottom > 950){
+      banana.remove();
+    }
 
   })
 }
@@ -220,6 +225,8 @@ if (gameOver === false){
 var makingInterval = setInterval(makeBanana, 2000);
 var bananaMover = setInterval(moveBanana, 5);
 var mushroomMover = setInterval(moveMush, 5);
+var colliderBanana = setInterval(bananaCollide, 1);
+var colliderMushroom = setInterval(mushroomCollide, 1);
 }
 
 if (gameOver === true){
@@ -228,9 +235,12 @@ if (gameOver === true){
 //this makes boo the mouse:
 $(document).on('mousemove', function(e){
 
+   
+
     if (score < 5){
       $('.boo1').show();
     $('.boo1').offset({left: e.pageX -15, top: e.pageY -15});
+
   }
  
   else if (score < 10){
@@ -251,7 +261,8 @@ $(document).on('mousemove', function(e){
   }
 
 
-    });
+  });
 
-});
+})
+
 
